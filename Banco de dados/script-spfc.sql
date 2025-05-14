bbe446c787b5c318bc041da941dacbcfabc4a500
muniz <gustavo.doliveira@sptech.school>
2025-05-14 12:06:45 -0300

    mudanca banco de dados

--- Banco de dados/script-spfc.sql
+++ Banco de dados/script-spfc.sql
@@ -1,8 +1,8 @@
-create database saopaulo;
+create database if not exists saopaulo;
 use saopaulo;
 
 create table usuario(
-id int primary key auto_increment,
+idUsuario int primary key auto_increment,
 nome varchar(100),
 email varchar(60),
 senha varchar(10)
@@ -18,7 +18,7 @@ id int primary key auto_increment,
 fkpergunta int,
 respostas varchar(100),
 corretas int,
-foreign key (fkpergunta) references perguntas(id)
+foreign key (fkpergunta) references pergunta(id)
 );
 
 create table resposta_usuario(
@@ -27,9 +27,11 @@ fkusuario int,
 fkpergunta int,
 fkresposta int,
 acertou int,
-foreign key (fkusuario) references usuario(id),
+foreign key (fkusuario) references usuario(idUsuario),
 foreign key (fkpergunta) references pergunta(id),
 foreign key(fkresposta) references resposta(id)
 );
+
+drop database saopaulo;
 
 
